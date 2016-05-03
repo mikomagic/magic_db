f89c04e9a83d545ae47ef00ac5b1a617aa2403c6
Michael Keller <nuxgu0@gmail.com>
2016-05-03 18:32:35 +0300

    update_all: last four sets seem to work

---
+++ update_all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+./update.py -lde ORI "Magic Origins"
+./update.py -lde BFZ "Battle for Zendikar"
+./update.py -lde OGW "Oath of the Gatewatch"
+./update.py -lde SOI "Shadows over Innistrad"
